a18af6403c312e011b9be5b06e12daef0b815a71
dishion <1535075571@qq.com>
2025-07-05 00:30:38 +0800

    初始化

---
+++ Core/Inc/main.h
@@ -0,0 +1,113 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define MPU6050_SDA_Pin GPIO_PIN_0
+#define MPU6050_SDA_GPIO_Port GPIOF
+#define MPU6050_SCL_Pin GPIO_PIN_1
+#define MPU6050_SCL_GPIO_Port GPIOF
+#define Right_A_Pin GPIO_PIN_0
+#define Right_A_GPIO_Port GPIOA
+#define Right_B_Pin GPIO_PIN_1
+#define Right_B_GPIO_Port GPIOA
+#define Left_PWMA_Pin GPIO_PIN_6
+#define Left_PWMA_GPIO_Port GPIOA
+#define Left_PWMB_Pin GPIO_PIN_7
+#define Left_PWMB_GPIO_Port GPIOA
+#define Right_PWMA_Pin GPIO_PIN_0
+#define Right_PWMA_GPIO_Port GPIOB
+#define Right_PWMb_Pin GPIO_PIN_1
+#define Right_PWMb_GPIO_Port GPIOB
+#define Screen_TX_Pin GPIO_PIN_10
+#define Screen_TX_GPIO_Port GPIOB
+#define Screen_RX_Pin GPIO_PIN_11
+#define Screen_RX_GPIO_Port GPIOB
+#define Left_A_Pin GPIO_PIN_12
+#define Left_A_GPIO_Port GPIOD
+#define Left_B_Pin GPIO_PIN_13
+#define Left_B_GPIO_Port GPIOD
+#define L_IN1_Pin GPIO_PIN_2
+#define L_IN1_GPIO_Port GPIOG
+#define L_IN2_Pin GPIO_PIN_3
+#define L_IN2_GPIO_Port GPIOG
+#define R_IN2_Pin GPIO_PIN_4
+#define R_IN2_GPIO_Port GPIOG
+#define R_IN1_Pin GPIO_PIN_5
+#define R_IN1_GPIO_Port GPIOG
+#define Camera_TX_Pin GPIO_PIN_9
+#define Camera_TX_GPIO_Port GPIOA
+#define Camera_RX_Pin GPIO_PIN_10
+#define Camera_RX_GPIO_Port GPIOA
+#define Bluetooth_TX_Pin GPIO_PIN_10
+#define Bluetooth_TX_GPIO_Port GPIOC
+#define Bluetooth_RX_Pin GPIO_PIN_11
+#define Bluetooth_RX_GPIO_Port GPIOC
+#define OLED_SCL_Pin GPIO_PIN_6
+#define OLED_SCL_GPIO_Port GPIOB
+#define OLED_SDA_Pin GPIO_PIN_7
+#define OLED_SDA_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/main.c
@@ -0,0 +1,269 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "i2c.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "MPU6050DMP.h"
+#include "Debug.h"
+#include "string.h"
+#include "get_speed.h"
+#include "pid.h"
+#include "motor.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+int times = 0;//视觉倍速定时
+int left_pwm = 0;
+int right_pwm = 0;
+int left_target = 100;
+int right_target= 100;
+int32_t L_actual = 0;
+int32_t R_actual = 0;
+float pitch,roll,yaw;
+
+float base_speed = 0.0; //正常速度
+float target_angle = 0.0;//正常角度
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if (htim == &htim9) {
+    //视觉模块定时触发
+    times = (times + 1) % 11;
+    if (times == 10) {
+      char data[]="1\n";
+      HAL_UART_Transmit_DMA(&huart1,(uint8_t*)data, strlen(data));
+    }
+
+    //速度获取
+    L_actual = get_speed(&htim4);
+    R_actual = get_speed(&htim5);
+    if (stop_flag != 1){
+      MPU6050_DMP_Get_Date(&pitch,&roll,&yaw);
+      //角度环
+      angle_ring.target = target_angle;
+      PID_update(angle_ring);
+      speed_ring_l.target = base_speed - angle_ring.out;
+      speed_ring_r.target = base_speed + angle_ring.out;
+      //速度环
+      speed_ring_l.actual = L_actual;
+      speed_ring_l.actual = R_actual;
+      PID_update(speed_ring_l);
+      PID_update(speed_ring_r);
+      //控制电机
+      Set_Pwml(speed_ring_l.out);
+      Set_Pwmr(speed_ring_r.out);
+    }
+    else {
+      Set_Pwml(0);
+      Set_Pwmr(0);
+    }
+  }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_I2C1_Init();
+  MX_I2C2_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  MX_TIM5_Init();
+  MX_UART4_Init();
+  MX_USART1_UART_Init();
+  MX_USART3_UART_Init();
+  MX_TIM9_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  //变量定义
+  //陀螺仪初始化
+  int ret;
+  do{
+    ret=MPU6050_DMP_init();
+  }while(ret);
+
+  //定时器初始化
+  HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_ALL); //开启编码器模式
+  HAL_TIM_Encoder_Start(&htim5, TIM_CHANNEL_ALL);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_ALL);//开启pwm
+  HAL_TIM_Base_Start_IT(&htim4);                  //开启编码器的中断
+  HAL_TIM_Base_Start_IT(&htim5);                  //开启编码器的中断
+  HAL_TIM_Base_Start_IT(&htim9);
+
+  //sofa接收初始化
+  HAL_UARTEx_ReceiveToIdle_IT(&huart2, sofa_data, sizeof(sofa_data));
+
+  //视觉接收初始化
+  HAL_UARTEx_ReceiveToIdle_DMA(&huart1, camera_data, sizeof(camera_data));
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
